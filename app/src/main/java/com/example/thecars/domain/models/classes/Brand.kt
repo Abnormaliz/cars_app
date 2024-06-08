252ddbf7169527929fa242a03978395cd65c5601
Pavel Kostrygin <p-kostrygin@yandex-team.ru>
2024-06-08 13:53:36 +0300

    brand class example

--- app/src/main/java/com/example/thecars/domain/models/classes/Brand.kt
+++ app/src/main/java/com/example/thecars/domain/models/classes/Brand.kt
@@ -2,36 +2,41 @@ package com.example.thecars.domain.models.classes
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-
+//domain
 data class Brand(
     val logo: Int,
     val name: String,
-    val modelList: List<Model>) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString() ?: "",
-        mutableListOf<Model>().apply {
-            parcel.readList(this, Model::class.java.classLoader)
-        })
+    val modelList: List<Model>
+)
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(logo)
-        parcel.writeString(name)
-        parcel.writeList(modelList)
+
+//presentation
+@Parcelize
+data class BrandUi(
+    val logo: Int,
+    val name: String,
+    val modelList: List<ModelUi>) : Parcelable
+
+fun Brand.toBrandUi(): BrandUi {
+    return BrandUi(
+        logo = this.logo,
+        name = this.name,
+        modelList = this.modelList
+    )
+}
+
+object BrandMapper {
+    fun toBrandUi(brand: Brand): BrandUi {
+        return BrandUi(
+            logo = brand.logo,
+            name = brand.name,
+            modelList = brand.modelList
+        )
     }
 
-    override fun describeContents(): Int {
-        return 0
-    }
+    fun toBrand(brand: BrandUi): Brand {
 
-    companion object CREATOR : Parcelable.Creator<Brand> {
-        override fun createFromParcel(parcel: Parcel): Brand {
-            return Brand(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Brand?> {
-            return arrayOfNulls(size)
-        }
     }
 }
